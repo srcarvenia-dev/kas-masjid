95d3458d48b304d7ba3300eb157612fd1c5aa375
srcarvenia-dev <srcarvenia@gmail.com>
2026-02-23 23:29:39 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener("install",e=>{
+ e.waitUntil(
+   caches.open("kas-masjid").then(c=>c.add("index.html"))
+ );
+});
+self.addEventListener("fetch",e=>{
+ e.respondWith(
+   caches.match(e.request).then(r=>r||fetch(e.request))
+ );
+});
